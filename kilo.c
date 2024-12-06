d80ddc059cfe28160f474a2de0729c2f34aa51b1
MedAzizGharbi <gharbi.aziz03@gmail.com>
2024-12-06 17:12:50 +0100

    fixed \0

--- kilo.c
+++ kilo.c
@@ -145,7 +145,7 @@ void editorAppendRow(char *s , size_t len){
 	E.row[at].size = len;
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars , s, len);
-	E.row[at].chars[len] = '\O';
+	E.row[at].chars[len] = '\0';
 	E.numrows++;
 }
 
